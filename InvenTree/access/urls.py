adcc0891a61394eff5081d32da7ae398c9f8cb46
ss-aman <aman001700@gmail.com>
2020-01-21 13:52:23 +0530

    access app

---
+++ InvenTree/access/urls.py
@@ -0,0 +1,27 @@
+"""
+URL lookup for Company app
+"""
+
+
+from django.conf.urls import url, include
+from django.views.generic.base import RedirectView
+
+from . import views
+
+
+urlpatterns = [
+    url(r'new/?', views.GroupsCreateView.as_view(), name='group-create'),
+    url(r'list/?', views.GroupsListView.as_view(), name='group-list'),
+
+    # # url(r'orders/?', views.CompanyDetail.as_view(template_name='company/orders.html'), name='company-detail-orders'),
+
+    # url(r'parts/?', views.CompanyDetail.as_view(template_name='company/detail_part.html'), name='company-detail-parts'),
+    # url(r'stock/?', views.CompanyDetail.as_view(template_name='company/detail_stock.html'), name='company-detail-stock'),
+    # url(r'purchase-orders/?', views.CompanyDetail.as_view(template_name='company/detail_purchase_orders.html'), name='company-detail-purchase-orders'),
+
+    # url(r'thumbnail/?', views.CompanyImage.as_view(), name='company-image'),
+
+    # # Any other URL
+    # url(r'^.*$', views.CompanyDetail.as_view(), name='company-detail'),
+]
+
--- InvenTree/access/views.py
+++ InvenTree/access/views.py
@@ -2,10 +2,29 @@ from django.shortcuts import render
 from django.views import generics
 from django.contrib.auth.model import Groups
 
+
+class GroupsListView(generics.ListView):
+    model = Groups
+    template_name = 'simple_list.html'
+    
+
 class GroupsCreateView(generics.CreateView):
     model = Groups
     template_name = 'simple_form.html'
-    success_url = 
+    success_url = '' # put reverse url of list view here
+
+
+class GroupsUpdateView(generics.UpdateView):
+    model = Groups
+    template_name = 'simple_form.html'
+    success_url = '' # put reverse url of list view here
+
+
+class GroupsDeleteView(generics.DeleteView):
+    model = Groups
+    template_name = 'simple_form.html'
+    success_url = '' # put reverse url of list view here
+
 
 
 
